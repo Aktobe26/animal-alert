f0492c95e2b7d967cd63c03b51c7f732e7d3e6dc
Тамерлан Жамалов <tamerlanzamalov@MacBook-Air-Tamerlan.local>
2026-01-14 15:54:08 +0500

    TTL changed to 30 minutes

--- components/Map.tsx
+++ components/Map.tsx
@@ -23,7 +23,7 @@ type AnimalRow = {
 
 /* ================= CONFIG ================= */
 
-const TTL_MS = 60 * 60 * 1000; // 60 минут
+const TTL_MS = 30 * 60 * 1000; // 30 минут
 
 const ICONS: Record<Animal, string> = {
   cow: '/icons/cow.png',
